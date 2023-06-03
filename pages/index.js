674b409b0cf92a1032ebe8b596d6b3a27c044840
Alessandro Pecorilla <93372660+JardiinWD@users.noreply.github.com>
2023-06-03 12:52:01 +0000

    Update della branch main

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
 
 export default function Home() {
-  return <h1>Hello World nuovamente da main</h1>;
+  return <h1>Ciao! Questo è un progetto Next su CodeSandbox</h1>;
 }
