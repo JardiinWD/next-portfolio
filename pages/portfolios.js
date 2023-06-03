8d606ab7685d40287ba8c5cf0aea523fe69c3b0a
Alessandro Pecorilla <93372660+JardiinWD@users.noreply.github.com>
2023-06-03 16:03:44 +0000

    [88] Fetch posts

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,12 +1,36 @@
 import BaseLayout from "../components/layouts/BaseLayout";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Portfolios = () => {
+const Portfolios = ({ posts }) => {
+  const renderPosts = () => {
+    return posts.map((item) => {
+      return <li key={item.id}>{item.id}</li>;
+    });
+  };
+
   return (
     <BaseLayout>
       <h1>I am Portfolio page</h1>
+      <ul>{renderPosts()}</ul>
     </BaseLayout>
   );
 };
 
 export default Portfolios;
+
+Portfolios.getInitialProps = async () => {
+  let posts = [];
+  try {
+    const axiosRes = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    posts = axiosRes.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  return {
+    posts: posts.slice(0, 10),
+  };
+};
